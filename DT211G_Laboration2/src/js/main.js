c0540ae0126ecb65e7a3d14d18affccf6d62861b
Hanna Lindkvist <hali2507@student.miun.se>
2026-01-26 11:14:07 +0100

    skriver funktion för hämta json-data

--- DT211G_Laboration2/src/js/main.js
+++ DT211G_Laboration2/src/js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log(`Hello world!!!!`);
+document.addEventListener("DOMContentLoaded", fetchData());
+
+async function fetchData() {
+    try {
+    const response = await fetch(
+        "https://webbutveckling.miun.se/files/ramschema.json"
+    );
+    const data = await response.json();
+    console.log(data);
+    } catch (error) {
+        console.error(`Felmeddelande ${error}`);
+    }
+}
