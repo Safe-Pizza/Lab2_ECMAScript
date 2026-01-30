2b023a29e7d3696653f171b385ad632eaba2f93e
Hanna Lindkvist <hali2507@student.miun.se>
2026-01-30 11:18:41 +0100

    skriver funktion writeTable

--- DT211G_Laboration2/src/js/main.js
+++ DT211G_Laboration2/src/js/main.js
@@ -10,8 +10,33 @@ async function fetchData(url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        writeTable(data);
     } catch (error) {
         console.error(`Felmeddelande ${error}`);
     }
+}
+
+function writeTable(tableData) {
+    const tableEl = document.querySelector("#tableData");
+
+    tableData.forEach(data => {
+        //skapa tr + td element
+        const trEl = document.createElement("tr");
+        const tdCodeEl = document.createElement("td");
+        const tdNameEl = document.createElement("td");
+        const tdProgEl = document.createElement("td");
+
+        //lägg till text
+        tdCodeEl.innerHTML = data.code;
+        tdNameEl.innerHTML = data.coursename;
+        tdProgEl.innerHTML = data.progression;
+
+        //lägger till td i tr
+        trEl.appendChild(tdCodeEl);
+        trEl.appendChild(tdNameEl);
+        trEl.appendChild(tdProgEl);
+
+        //skriv ut till DOM
+        tableEl.appendChild(trEl);
+    });
 }
